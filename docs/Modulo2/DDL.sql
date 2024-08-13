0857a0a86669343c462ea80502aa8a898b9f2e91
Julia Gabriela <149431356+JuliaGabP@users.noreply.github.com>
2024-08-13 12:29:30 -0300

    Update DDL.sql

--- docs/Modulo2/DDL.sql
+++ docs/Modulo2/DDL.sql
@@ -1,71 +1,71 @@
 BEGIN;
 
-CREATE TABLE Personagem (
-    ID SERIAL PRIMARY KEY,
-    Nome VARCHAR(50) NOT NULL,
-    DescricaoFisica TEXT,
-    Forca INT CHECK (Forca >= 0),
-    Agilidade INT CHECK (Agilidade >= 0),
-    Inteligencia INT CHECK (Inteligencia >= 0),
-    VidaInicial INT CHECK (VidaInicial > 0)
+CREATE TABLE personagem (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(30) NOT NULL,
+    descricao_fisica TEXT NOT NULL,
+    forca INT CHECK (forca >= 0) NOT NULL,
+    agilidade INT CHECK (agilidade >= 0) NOT NULL,
+    inteligencia INT CHECK (inteligencia >= 0) NOT NULL,
+    vida_inicial INT CHECK (vida_inicial > 0) NOT NULL
 );
 
-CREATE TABLE Mundo (
-    ID SERIAL PRIMARY KEY,
-    Nome VARCHAR(50) NOT NULL,
-    Dificuldade INT CHECK (Dificuldade IN (1, 2, 3)),
-    Status VARCHAR(20),
-    ID_Personagem INT REFERENCES Personagem(ID),
-    VidaAtual INT CHECK (VidaAtual > 0),
-    FaseAtual INT,
-    MoedasColetadas INT DEFAULT 0,
-    FantasmasDerrotados INT DEFAULT 0
+CREATE TABLE mundo (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(30) NOT NULL,
+    dificuldade INT CHECK (dificuldade IN (1, 2, 3)) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    id_personagem INT REFERENCES personagem(ID) NOT NULL,
+    vida_atual INT CHECK (vida_atual > 0) NOT NULL,
+    fase_atual INT NOT NULL,
+    moedas_coletadas INT DEFAULT 0 NOT NULL,
+    fantasmas_derrotados INT DEFAULT 0 NOT NULL
 );
 
-CREATE TABLE Fase (
-    ID SERIAL PRIMARY KEY,
-    Nome VARCHAR(50) NOT NULL,
-    Descricao TEXT,
-    Ordem INT,
-    ID_Mundo INT REFERENCES Mundo(ID),
-    CoisaRuim TEXT
+CREATE TABLE fase (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(30) NOT NULL,
+    descricao TEXT NOT NULL,
+    ordem INT NOT NULL,
+    id_mundo INT REFERENCES mundo(id) NOT NULL,
+    coisa_ruim TEXT
 );
 
-CREATE TABLE Sala (
-    ID SERIAL PRIMARY KEY,
-    Nome VARCHAR(50) NOT NULL,
-    Descricao TEXT,
-    ID_Fase INT REFERENCES Fase(ID),
-    Ordem INT,
-    VidaRestaurada INT CHECK (VidaRestaurada BETWEEN 4 AND 8)
+CREATE TABLE sala (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(30) NOT NULL,
+    descricao TEXT NOT NULL,
+    id_fase INT REFERENCES fase(id) NOT NULL,
+    ordem INT NOT NULL,
+    vida_restaurada INT CHECK (vida_restaurada BETWEEN 4 AND 8)
 );
 
-CREATE TABLE Arma (
-    ID SERIAL PRIMARY KEY,
-    Nome VARCHAR(50) NOT NULL,
-    DescricaoFisica TEXT,
-    DanoMedio INT CHECK (DanoMedio > 0),
-    DescricaoAtaque TEXT,
-    Tipo VARCHAR(20) CHECK (Tipo IN ('Forca', 'Agilidade', 'Inteligencia'))
+CREATE TABLE arma (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(30) NOT NULL,
+    descricao_fisica TEXT NOT NULL,
+    dano_medio INT CHECK (dano_medio > 0) NOT NULL,
+    descricao_ataque TEXT NOT NULL,
+    tipo VARCHAR(20) CHECK (tipo IN ('forca', 'agilidade', 'inteligencia')) NOT NULL
 );
 
-CREATE TABLE PersonagemArma (
-    ID SERIAL PRIMARY KEY,
-    ID_Personagem INT REFERENCES Personagem(ID),
-    ID_Arma INT REFERENCES Arma(ID)
+CREATE TABLE personagemarma (
+    id SERIAL PRIMARY KEY,
+    id_personagem INT REFERENCES personagem(id) NOT NULL,
+    id_arma INT REFERENCES arma(id) NOT NULL
 );
 
-CREATE TABLE Fantasma (
-    ID SERIAL PRIMARY KEY,
-    Nome VARCHAR(50) NOT NULL,
-    Descricao TEXT,
-    Vida INT CHECK (Vida > 0),
-    AtaqueEspecial TEXT,
-    Barulhos TEXT,
-    DropaMoeda BOOLEAN,
-    Ordem INT,
-    Dica TEXT,
-    ID_Sala INT REFERENCES Sala(ID)
+CREATE TABLE fantasma (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(30) NOT NULL,
+    descricao TEXT NOT NULL,
+    vida INT CHECK (vida > 0) NOT NULL,
+    ataque_especial TEXT NOT NULL,
+    barulhos TEXT NOT NULL,
+    dropa_moeda BOOLEAN NOT NULL,
+    ordem INT NOT NULL,
+    dica TEXT NOT NULL,
+    id_sala INT REFERENCES sala(id) NOT NULL
 );
 
 COMMIT;
